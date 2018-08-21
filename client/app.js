f12c757b468993e3fd0cdad2582ac862fc24e940
fjl冯觉良 <jl_feng@ctrip.com>
2018-08-21 17:09:56 +0800

    加上注释

--- client/app.js
+++ client/app.js
@@ -10,11 +10,13 @@ const render = Component => {
 }
 
 render(App)
-
+// 在Dev环境并且devServer.hot = true
 if (module.hot) {
+  // app.jsx更新了
   module.hot.accept('./App.jsx', () => {
+    // require更新后的app.jsx 重新render
     const NextApp  = require('./App.jsx').default
     render(NextApp)
-    console.log('---hot---')
+    console.log('---HMR work---')
   })
 }
